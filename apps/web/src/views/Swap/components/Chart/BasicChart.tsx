4b5795c8b1590f5562ee3ab500a9410b1a8d997b
Alireza <alireza.rzna@gmail.com>
2023-02-06 14:32:07 +0400

    init

---
+++ apps/web/src/views/Swap/components/Chart/BasicChart.tsx
@@ -0,0 +1,163 @@
+import { Box, ButtonMenu, ButtonMenuItem, Flex, IconButton, SyncAltIcon, Text } from '@pancakeswap/uikit'
+import { useTranslation } from '@pancakeswap/localization'
+import { useState, memo } from 'react'
+import { useFetchPairPrices } from 'state/swap/hooks'
+import dynamic from 'next/dynamic'
+import { PairDataTimeWindowEnum } from 'state/swap/types'
+import NoChartAvailable from './NoChartAvailable'
+import PairPriceDisplay from '../../../../components/PairPriceDisplay'
+import { getTimeWindowChange } from './utils'
+import { DoubleCurrencyLogo, CurrencyLogo } from 'components/Logo'
+
+const SwapLineChart = dynamic(() => import('./SwapLineChart'), {
+  ssr: false,
+})
+
+const BasicChart = ({
+  token0Address,
+  token1Address,
+  isChartExpanded,
+  inputCurrency,
+  outputCurrency,
+  isMobile,
+  currentSwapPrice,
+  onSwitchTokens
+}) => {
+  const [timeWindow, setTimeWindow] = useState<PairDataTimeWindowEnum>(0)
+
+  const { pairPrices = [], pairId } = useFetchPairPrices({
+    token0Address,
+    token1Address,
+    timeWindow,
+    currentSwapPrice,
+  })
+  const [hoverValue, setHoverValue] = useState<number | undefined>()
+  const [hoverDate, setHoverDate] = useState<string | undefined>()
+  const valueToDisplay = hoverValue || pairPrices[pairPrices.length - 1]?.value
+  const { changePercentage, changeValue } = getTimeWindowChange(pairPrices)
+  const isChangePositive = changeValue >= 0
+  const chartHeight = isChartExpanded ? 'calc(100vh - 220px)' : '378px'
+  const {
+    t,
+    currentLanguage: { locale },
+  } = useTranslation()
+  const currentDate = new Date().toLocaleString(locale, {
+    year: 'numeric',
+    month: 'short',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+  })
+
+  // Sometimes we might receive array full of zeros for obscure tokens while trying to derive data
+  // In that case chart is not useful to users
+  const isBadData =
+    pairPrices &&
+    pairPrices.length > 0 &&
+    pairPrices.every(
+      (price) => !price.value || price.value === 0 || price.value === Infinity || Number.isNaN(price.value),
+    )
+
+  if (isBadData) {
+    return (
+      <NoChartAvailable
+        token0Address={token0Address}
+        token1Address={token1Address}
+        pairAddress={pairId}
+        isMobile={isMobile}
+      />
+    )
+  }
+
+  return (
+    <>
+      <Flex
+        flexDirection={['column', null, null, null, null, null, 'row']}
+        alignItems={['flex-start', null, null, null, null, null, 'center']}
+        justifyContent="space-between"
+        px="24px"
+        flexWrap="wrap"
+      >
+        <Flex flexDirection="column" pt="12px">
+          <PairPriceDisplay
+            value={pairPrices?.length > 0 && valueToDisplay}
+            inputSymbol={inputCurrency?.symbol}
+            outputSymbol={outputCurrency?.symbol}
+          >
+            <Text color={isChangePositive ? '#2660D0' : 'failure'} fontSize="14px" ml="4px" bold>
+              {`${isChangePositive ? '+' : ''}${changeValue.toFixed(3)} (${changePercentage}%)`}
+            </Text>
+          </PairPriceDisplay>
+          <Text small color="#2660D0">
+            {hoverDate || currentDate}
+          </Text>
+        </Flex>
+        <Flex alignItems="center">
+          {outputCurrency ? (
+            <DoubleCurrencyLogo currency0={inputCurrency} currency1={outputCurrency} size={24} margin />
+          ) : (
+            inputCurrency && <CurrencyLogo currency={inputCurrency} size="24px" style={{ marginRight: '8px' }} />
+          )}
+          {inputCurrency && (
+            <Text color="text" bold>
+              {outputCurrency ? `${inputCurrency.symbol}/${outputCurrency.symbol}` : inputCurrency.symbol}
+            </Text>
+          )}
+          <IconButton variant="text" onClick={onSwitchTokens}>
+            <SyncAltIcon ml="6px" color="primary" />
+          </IconButton>
+          {/* {isDesktop && (
+            <Flex>
+              <ChartButton
+                aria-label={t('Basic')}
+                title={t('Basic')}
+                $active={chartView === ChartViewMode.BASIC}
+                scale="sm"
+                variant="text"
+                color="primary"
+                mr="8px"
+                disabled
+              >
+                {t('Basic')}
+              </ChartButton>
+              <ChartButton
+                aria-label="TradingView"
+                title="TradingView"
+                $active={chartView === ChartViewMode.TRADING_VIEW}
+                scale="sm"
+                variant="text"
+                onClick={() => setChartView(ChartViewMode.TRADING_VIEW)}
+              >
+                {isDesktop ? 'TradingView' : <TradingViewIcon color="primary" />}
+              </ChartButton>
+            </Flex>
+          )} */}
+        </Flex>
+      </Flex>
+      <Box height={isMobile ? '100%' : chartHeight} p={isMobile ? '0px' : '16px'} width="100%">
+        <SwapLineChart
+          data={pairPrices}
+          setHoverValue={setHoverValue}
+          setHoverDate={setHoverDate}
+          isChangePositive={isChangePositive}
+          timeWindow={timeWindow}
+        />
+      </Box>
+    </>
+  )
+}
+
+export default memo(BasicChart, (prev, next) => {
+  return (
+    prev.token0Address === next.token0Address &&
+    prev.token1Address === next.token1Address &&
+    prev.isChartExpanded === next.isChartExpanded &&
+    prev.isMobile === next.isMobile &&
+    prev.isChartExpanded === next.isChartExpanded &&
+    ((prev.currentSwapPrice !== null &&
+      next.currentSwapPrice !== null &&
+      prev.currentSwapPrice[prev.token0Address] === next.currentSwapPrice[next.token0Address] &&
+      prev.currentSwapPrice[prev.token1Address] === next.currentSwapPrice[next.token1Address]) ||
+      (prev.currentSwapPrice === null && next.currentSwapPrice === null))
+  )
+})
